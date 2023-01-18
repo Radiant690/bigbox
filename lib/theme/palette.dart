cb1a7e4e90e4f593e48f946dc649654d88a9b5b7
Venkatasai Kadamati <venkatasaikadamati@gmail.com>
2023-01-18 14:37:05 +0530

    Added custom_theme.dart & Palette.dart

---
+++ lib/theme/palette.dart
@@ -0,0 +1,7 @@
+// ? Contains all the colors
+import 'package:flutter/rendering.dart';
+
+class Palette {
+  // ! Color is a class which takes an parameter such as 0xff040B13, it acts as a parametized constructor and initiates the new object variable almostBlack.
+  static const Color almostBlack = Color(0xff040B13);
+}
